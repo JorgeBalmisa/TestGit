ea4918d81930725fb1a4014ebde77acfaa972175
JorgeBalmi <palomaclase41@gmail.com>
2023-04-18 13:43:20 +0200

    Commit2

--- src/iespaloma/TestGit.java
+++ src/iespaloma/TestGit.java
@@ -3,8 +3,9 @@ package iespaloma;
 public class TestGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("UwU");
+		System.out.println("OwO");
 	}
 
 }
